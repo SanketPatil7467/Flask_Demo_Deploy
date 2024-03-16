d7585fb98a960e4621a3dfe1df2e210bc4fd6847
Sanket <sanketpatil7467@gmail.com>
2024-03-16 13:00:24 +0530

    second commit

--- app.py
+++ app.py
@@ -4,4 +4,9 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return render_template('index.html') 
+    return render_template('index.html')
+
+
+@app.route("/home")
+def home():
+    return render_template('home.html')
