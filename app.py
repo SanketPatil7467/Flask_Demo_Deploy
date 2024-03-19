d3e860e3ada3713c94a8b424a6168d90d8bf33dc
Sanket <sanketpatil7467@gmail.com>
2024-03-19 12:17:01 +0530

    commit15

--- app.py
+++ app.py
@@ -22,14 +22,14 @@ def returnJSON():
 
 @app.route("/create_new_object/<string:obj_name>")
 def createObject(obj_name):
-    my_dict = {}
-    with open('db.txt', 'r') as file:
-        data = file.read()
-        my_dict = json.loads(data)
-        my_dict[obj_name] = {}
-    with open('db.txt', 'w') as file:
-        json.dump(my_dict, file)
-    return "Object created successfully !"
+    # my_dict = {}
+    # with open('db.txt', 'r') as file:
+    #     data = file.read()
+    #     my_dict = json.loads(data)
+    #     my_dict[obj_name] = {}
+    # with open('db.txt', 'w') as file:
+    #     json.dump(my_dict, file)
+    return f"Object created successfully {obj_name}!"
 
 @app.route("/count/<int:n>")
 def calculation(n):
